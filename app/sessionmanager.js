e2ea7548873c33fbf76eeaf05fc4546240249f2e
dev <dev@emuaki.com>
2011-12-17 07:09:28 +0000

    app/.

---
+++ app/sessionmanager.js
@@ -0,0 +1,51 @@
+var SessionManager = function(option){
+    this.initialize(option);
+};
+
+SessionManager.prototype = {
+    
+    sessions : {},
+    
+    initialize : function(option){
+        this.io = option.io;
+        this.pricePublisher = option.pricePublisher;
+        this.barPublisher = option.barPublisher;
+        this.connectionCount = 0;
+    },
+    
+    start : function(){
+        var self = this;
+        self.io.sockets.on('connection', function (socket) {
+
+            self.connectionCount++;
+            self.addSession(new ClientSession({socket: socket, sessionManager : self}));
+            socket.broadcast.emit('connectionCountChange', { 
+                message : 'connected',
+                connectionCount: self.connectionCount
+            });
+            
+            socket.on('disconnect', function(){
+                self.removeSession(socket.id);
+                socket.broadcast.emit('connectionCountChange', { 
+                    message : 'disconnected',
+                    connectionCount: self.connectionCount
+                });
+            });
+        });
+        
+    },
+    
+    addSession : function(session){
+        this.sessions[session.id] = session;
+    },
+    
+    removeSession : function(id){
+        delete this.sessions[id];
+    }
+};
+
+exports.create = function(){
+    return new SessionManager(arguments[0], arguments[1]);   
+};
+
+
