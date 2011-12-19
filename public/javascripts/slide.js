80d056ead3c1ef5aee80853050761a5b6537ca6f
dev <dev@emuaki.com>
2011-12-19 11:00:01 +0000

    modify

---
+++ public/javascripts/slide.js
@@ -0,0 +1,47 @@
+var SlideControl = function(args){
+    this.initialize(args);
+};
+
+SlideControl.prototype = {
+
+    isReady : true,
+    
+    initialize : function(args){
+        this.admin = args.admin;
+        this.socket = args.socket;
+        this.setupListener();
+        this.setupDeck();
+    },
+    
+    setupDeck : function(){
+        $.deck('.slide');
+        if(! admin){
+            $.deck('extend', 'next', function(){});
+            $.deck('extend', 'prev', function(){});
+        }               
+    },
+    
+    setupListener : function(){
+        var self = this;
+        $(document).bind('deck.change', function(event, from, to) {
+            self.myPageChange(from , to);
+        });      
+        this.socket.on("SlideSession-pageChange", function(data){
+            self.onPageChange(data);
+        });
+    },
+    
+    myPageChange : function(from ,to){
+        if(this.isReady) {
+            console.log("from:" + from  + ", to:" + to);
+            socket.emit("SlideSession-pageChange", {page : to} );
+        }
+        this.isReady = true;
+    },
+    
+    onPageChange : function(data){
+        this.isReady = false;
+        $.deck('go', data.page);    
+    }
+
+};
