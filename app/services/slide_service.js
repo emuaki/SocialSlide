0335804fe904550cb0b2926d655871f41dbe189a
dev <dev@emuaki.com>
2011-12-20 13:52:35 +0000

    modify

---
+++ app/services/slide_service.js
@@ -0,0 +1,23 @@
+
+var SlideService = function(args){
+    this.initialize(args);
+};
+
+SlideService.prototype = {
+    
+    pageNo : 0,
+    
+    initialize : function(args){
+        
+    },
+    
+    change : function(pageNo){
+        this.pageNo = pageNo;
+    }
+    
+};
+
+var slideService = new SlideService();
+exports.getSlideService = function(){
+    return slideService;   
+};
