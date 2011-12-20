3c0c4ea86e47310cb424c3412b1c531cb9181d0b
dev <dev@emuaki.com>
2011-12-20 14:56:52 +0000

    modify

---
+++ public/javascripts/status.js
@@ -0,0 +1,46 @@
+var ConnectionStatus = function(args){
+    this.initialize(args);  
+};
+
+ConnectionStatus.prototype = {
+    
+    initialize : function(args){
+        this.socket = args.socket;
+        this.status = $(args.selector.status);
+        this.count = $(args.selector.count);
+        this.transport = $(args.selector.transport);
+    },
+    
+    start : function(){        
+        var self = this;
+        this.socket.on('connecting', function(){
+            self.status.html('connecting');
+        });
+        
+        this.socket.on('connect', function(){
+            self.status.html('connect');
+        });
+
+        this.socket.on('connect_faild', function(){
+            self.status.html('connect_faild');
+        });
+
+        this.socket.on('reconnecting', function(){
+            self.status.html('reconnecting');
+        });
+        
+        this.socket.on('reconnect_faild', function(){
+            self.status.html('reconnect_faild');
+        });
+
+        this.socket.on('disconnect', function(){
+            self.status.html('disconnect');
+        });
+                
+        this.socket.on('connectionCountChange', function (data) {
+            self.count.html(data.connectionCount);
+        });
+
+    }
+    
+};
