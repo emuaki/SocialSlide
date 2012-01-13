f8c24e32978e1e792b76521ab62012c055633600
dev <dev@emuaki.com>
2012-01-13 11:17:40 +0000

    modify

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 require.paths.push('app');
-require.paths.push('node_modules/socket.io-client/lib');
+require.paths.push('node_modules/socket.io-client/');
 console.log(require.paths);
 require('bootstrap'); 
