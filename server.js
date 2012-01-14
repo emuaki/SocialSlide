4e26cbc81c50cf27df75f9513ebb12f4b100104b
dev <dev@emuaki.com>
2012-01-14 06:39:46 +0000

    modify

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
 require.paths.push('app');
 require.paths.push('node_modules');
 console.log(require.paths);
-// require('bootstrap'); 
-
-var http = require('http');
- 
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello world\n");
-});
- 
-server.listen(process.env.PORT || 8001);
-require('socket.io');
+require('bootstrap'); 
