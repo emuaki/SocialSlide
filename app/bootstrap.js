1475e092ebb2fb981ada550492fb18718138012c
杉浦 正明 <masaaki@MBA001.(none)>
2012-05-03 16:47:42 +0900

    ファイル整理

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -5,20 +5,21 @@ app.listen(port);
 console.log("Express server listening on port %d", app.address().port);
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/../public'));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.set('view options', {layout: false});
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/../public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+    app.use(express.errorHandler()); 
 });
 
 io.configure('production', function(){
